f2e22086c461ab3d0368f728359e69cc32689dcf
Yulia <yulia.scherbina@gmail.com>
2018-04-17 11:44:03 +0200

    add TodoList

--- todo-list/src/TodoList.js
+++ todo-list/src/TodoList.js
@@ -2,12 +2,30 @@ import React, { Component } from 'react';
 import './style.css';
 
 export default class TodoList extends Component {
+  state = {
+    items: []
+  }
+
+  addItem = (event) => {
+    let itemArray = this.state.items;
+
+    itemArray.push({
+      text: this._inputElement.value,
+      key: Date.now()
+    });
+
+    this.setState({ items: itemArray });
+    event.preventDefault;
+  }
+
   render() {
     return (
       <div className="todo-list">
           <div className="header">
-            <form>
-              <input placeholder="enter task" />
+            <form onSubmit={this.addItem}>
+              <input 
+                  ref={(a) => this._inputElement = a}
+                  placeholder="enter task" />
               <button type="submit">add</button>
             </form>
           </div>
